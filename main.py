86441701a51006f45555f7ec591855f322d4ddda
Joel Aro <joelaro.to@gmail.com>
2024-01-27 12:14:45 -0500

    Wrap script in main

--- main.py
+++ main.py
@@ -3,17 +3,21 @@
 import os
 import shutil
 
-APP_DATA_PATH = os.getenv('LOCALAPPDATA')
-BASE_PATH = "Pal\Saved\SaveGames"
-SAVE_PATH = "\\".join([APP_DATA_PATH, BASE_PATH])
+def main():
+    APP_DATA_PATH = os.getenv('LOCALAPPDATA')
+    BASE_PATH = "Pal\Saved\SaveGames"
+    SAVE_PATH = "\\".join([APP_DATA_PATH, BASE_PATH])
 
-DESKTOP_PATH = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
-OUTPUT_PATH = "\\".join([DESKTOP_PATH, "palworld-save-export"])
+    DESKTOP_PATH = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
+    OUTPUT_PATH = "\\".join([DESKTOP_PATH, "palworld-save-export"])
 
-print(f"OUTPUT_PATH: {OUTPUT_PATH}.zip")
-print(f"SAVE_PATH: {SAVE_PATH}")
+    print(f"OUTPUT_PATH: {OUTPUT_PATH}.zip")
+    print(f"SAVE_PATH: {SAVE_PATH}")
 
-shutil.make_archive(OUTPUT_PATH, "zip", SAVE_PATH)
-print('DONE! .zip file created at OUTPUT_PATH above.')
+    shutil.make_archive(OUTPUT_PATH, "zip", SAVE_PATH)
+    print('DONE! .zip file created at OUTPUT_PATH above.')
 
-input("Press enter to exit;")
+    input("Press enter to exit;")
+
+if __name__ == '__main__':
+    main()
